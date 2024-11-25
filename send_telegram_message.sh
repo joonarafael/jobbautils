c3643decd607725c356ecebc20954c5ddc21b4e2
Joona Kettunen <joona.kettunen21@gmail.com>
2024-11-25 16:01:12 +0200

    i'm getting a migraine from this

--- send_telegram_message.sh
+++ send_telegram_message.sh
@@ -2,18 +2,31 @@
 
 # escape markdown special characters
 escape_markdown() {
-  echo "$1" | sed -E 's/([_*\[\]()~`>#+\-=|{}.!])/\\\1/g'
+  local input="$1"
+  local output="$input"
+
+  local markdown_chars=('_' '*' '[' ']' '(' ')' '~' '`' '>' '#' '+' '-' '=' '|' '{' '}' '.' '!')
+
+  for char in "${markdown_chars[@]}"; do
+    output="${output//"$char"/\\$char}"
+  done
+
+  echo "$output"
 }
 
 send_telegram_message() {
   local chat_id=$1
   local api_key=$2
   local message=$3
-
+  
   local escaped_message=$(escape_markdown "$message")
 
-  curl -X POST \
-    -H 'Content-Type: application/json' \
+  local response=$(curl -s -X POST \
+    -H "Content-Type: application/json" \
     -d "{\"chat_id\": \"$chat_id\", \"text\": \"$escaped_message\", \"parse_mode\": \"MarkdownV2\"}" \
-    "https://api.telegram.org/bot$api_key/sendMessage"
+    "https://api.telegram.org/bot$api_key/sendMessage")
+
+  echo "Telegram API response: $response"
 }
+
+send_telegram_message $1 $2 $3
